f08b673ac1f0d6550543bdbae6686f520ff69e13
Ishmael Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2021-11-13 11:34:24 -0500

    Removed unnecessary code regarding new_tab tokens

--- Converter.java
+++ Converter.java
@@ -76,11 +76,6 @@ public class Converter {
 //                            i++;
 //                        }
                         break;
-                    case NEW_TAB:
-                        //This is intended to handle new_tab characters
-                        pythonText += tokenList.get(i).lexeme;
-                        break;
-
                     case IMPORT:
                         //currently, there are no supported libraries to import so this
                         //will exist exclusively to skip all contents that follow import
